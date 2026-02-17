9351af15079d9d686f56d1c6566eecdccc455b72
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 21:58:28 +0530

    Update variables.tf

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,24 +1,32 @@
 variable "name" {
-  type = string
+  type    = string
+  default = "prod-ap-south-1"
 }
 
 variable "cidr" {
-  type = string
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "azs" {
-  type = list(string)
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b"]
 }
 
 variable "public_subnets" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.0.0/20", "10.0.16.0/20"]
 }
 
 variable "private_subnets" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.128.0/20", "10.0.144.0/20"]
 }
 
 variable "tags" {
   type    = map(string)
-  default = {}
+  default = {
+    Environment = "prod"
+    Platform    = "eks"
+  }
 }
