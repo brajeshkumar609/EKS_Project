b9f3b97fe7ede1fabfc5005868d557ec5a7c6923
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 22:59:45 +0530

    Update outputs.tf

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -1,24 +1,18 @@
+#############################################
+# VPC Module - Outputs
+#############################################
+
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "ID of the VPC."
   value       = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-  description = "IDs of private subnets"
+  description = "IDs of the private subnets."
   value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-  description = "IDs of public subnets"
+  description = "IDs of the public subnets."
   value       = module.vpc.public_subnets
-}
-
-output "private_route_table_ids" {
-  description = "Private route table IDs (optional, useful for debugging)"
-  value       = try(module.vpc.private_route_table_ids, [])
-}
-
-output "public_route_table_ids" {
-  description = "Public route table IDs (optional)"
-  value       = try(module.vpc.public_route_table_ids, [])
 }
