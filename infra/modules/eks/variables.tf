d3a54e70ac3ef0b754e76f299add91be6ba5acfb
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 21:18:42 +0530

    Create variables.tf

---
+++ infra/modules/eks/variables.tf
@@ -0,0 +1,7 @@
+variable "cluster_name"        { type = string }
+variable "kubernetes_version"  { type = string }
+variable "vpc_id"              { type = string }
+variable "private_subnet_ids"  { type = list(string) }
+variable "kms_key_arn"         { type = string }
+variable "admin_role_arn"      { type = string }
+variable "tags"                { type = map(string) default = {} }
