2f8ab7e64ff6ef405e20b301297be37b5df0cd1c
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 22:48:29 +0530

    Update eks.tf

--- infra/prod/eks.tf
+++ infra/prod/eks.tf
@@ -1,15 +1,15 @@
 module "eks" {
   source = "../modules/eks"
 
-  cluster_name        = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
-  vpc_id              = module.vpc.vpc_id
-  private_subnet_ids  = module.vpc.private_subnets
+  cluster_name        = "prod-eks"
+  kubernetes_version  = "1.29"
 
-  # TODO: replace with your KMS key for secrets encryption
-  kms_key_arn   = "arn:aws:kms:ap-south-1:123456789012:key/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  # TODO: replace with your platform admin role
-  admin_role_arn = "arn:aws:iam::123456789012:role/platform-eks-admin"
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnets
+
+  # KMS off (minimal cost)
+  kms_key_arn   = ""
+  admin_role_arn = "" # optional; leave empty
 
   tags = { Environment = "prod", Platform = "eks" }
 }
