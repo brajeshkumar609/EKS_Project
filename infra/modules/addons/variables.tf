72319089239f4948ad8b9fcaba5bd98dadac85d6
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 21:20:18 +0530

    Create variables.tf

---
+++ infra/modules/addons/variables.tf
@@ -0,0 +1,6 @@
+variable "cluster_name"        { type = string }
+variable "region"              { type = string }
+variable "vpc_id"              { type = string }
+variable "oidc_provider_arn"   { type = string }
+variable "alb_chart_version"   { type = string }
+variable "tags"                { type = map(string) default = {} }
