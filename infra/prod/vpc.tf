ae133be9b7161932de339c563cbf1cef56d79de9
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 23:04:07 +0530

    Update vpc.tf

--- infra/prod/vpc.tf
+++ infra/prod/vpc.tf
@@ -10,6 +10,3 @@ module "vpc" {
   tags = { Environment = "prod", Platform = "eks" }
 }
 
-output "vpc_id"             { value = module.vpc.vpc_id }
-output "private_subnet_ids" { value = module.vpc.private_subnets }
-output "public_subnet_ids"  { value = module.vpc.public_subnets }
