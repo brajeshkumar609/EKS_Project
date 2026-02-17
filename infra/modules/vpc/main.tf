038c20d9b3e681884af5dc4910f0e75ed9b467b2
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 21:16:43 +0530

    Create main.tf

---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.name
+  cidr = var.cidr
+
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway       = true
+  one_nat_gateway_per_az   = true
+  enable_dns_hostnames     = true
+  enable_dns_support       = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = var.tags
+}
