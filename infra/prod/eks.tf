eb60afc738ea16373cf424340475bd24ae832f75
EC2 Default User <ec2-user@ip-172-31-93-204.ec2.internal>
2026-02-18 05:06:27 +0000

    latest

--- infra/prod/eks.tf
+++ infra/prod/eks.tf
@@ -1,14 +1,14 @@
 module "eks" {
   source = "../modules/eks"
 
-  cluster_name        = "prod-eks"
-  kubernetes_version  = "1.29"
+  cluster_name       = "prod-eks"
+  kubernetes_version = "1.29"
 
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnets
 
   # KMS off (minimal cost)
-  kms_key_arn   = ""
+  kms_key_arn    = ""
   admin_role_arn = "" # optional; leave empty
 
   tags = { Environment = "prod", Platform = "eks" }
